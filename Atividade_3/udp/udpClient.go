fcf31277658526b2ab4f1c5693a6498400124b7a
André Luiz <alps2@cin.ufpe.br>
2022-08-21 21:47:22 -0300

    new app logic - udp

--- Atividade_3/udp/udpClient.go
+++ Atividade_3/udp/udpClient.go
@@ -1,50 +1,47 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
+        "fmt"
+        "net"
+        "os"
+		"strconv"
 )
 
 func main() {
-	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("Please provide host:port.")
-		return
-	}
+        arguments := os.Args
+        if len(arguments) == 1 {
+                fmt.Println("Please provide a host:port string")
+                return
+        }
+        CONNECT := arguments[1]
 
-	fmt.Println("Starting TCP connection at " + arguments[1])
+        s, err := net.ResolveUDPAddr("udp4", CONNECT)
+        c, err := net.DialUDP("udp4", nil, s)
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
 
-	CONNECT := arguments[1]
-	s, err := net.ResolveUDPAddr("udp4", CONNECT)
-	c, err := net.DialUDP("udp4", nil, s)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	defer c.Close()
-
-	var receivingExpression = ""
-
-	for {
-		fmt.Fprintf(c, "sendNextLetter\n")
-
-		buffer := make([]byte, 1024)
-		n, _, err := c.ReadFromUDP(buffer)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		var message = string(buffer[0:n])
+        fmt.Printf("The UDP server is %s\n", c.RemoteAddr().String())
+        defer c.Close()
 		
-		if message == "END" {
-			fmt.Println("Message received fully. UDP client exiting...")
-			return
-		}
+		var tries = 10000
+        for i := 0; i < tries; i++ {
+                data := []byte("sendTimeBetween\n")
+                _, err = c.Write(data)
 
-		var nextLetter = string(message[0])
+                if err != nil {
+                        fmt.Println(err)
+                        return
+                }
 
-		receivingExpression = receivingExpression + nextLetter
-		fmt.Println("->: " + receivingExpression)
-	}
+                buffer := make([]byte, 1024)
+                n, _, err := c.ReadFromUDP(buffer)
+                if err != nil {
+                        fmt.Println(err)
+                        return
+                }
+                fmt.Printf((strconv.Itoa(i) + "º Reply: %s\n"), string(buffer[0:n]))
+        }
 }
+    
--- Atividade_3/udp/udpServer.go
+++ Atividade_3/udp/udpServer.go
@@ -1,79 +1,52 @@
 package main
 
 import (
-	"fmt"
-	"net"
-	"os"
-	"strconv"
+        "fmt"
+        "net"
+        "os"
+        "strconv"
+        "time"
 )
 
-var sendingExpression = "socorram me subi no onibus em marrocos"
-var sendingExpressionLength = len(sendingExpression)
-var progressTracker map[int]int
-var clientIdCounter = 0
-
-func handleConnection(connection *net.UDPConn, quit chan struct{}) {
-	buffer := make([]byte, 1024)
-	n, remoteAddr, err := 0, new(net.UDPAddr), error(nil)
-
-	clientIdCounter++
-	var id = clientIdCounter
-	progressTracker[id] = 0
-	fmt.Println("New client connection established. id: " + strconv.Itoa(id))
-
-	for err == nil {
-		n, remoteAddr, err = connection.ReadFromUDP(buffer)
-		var message = string(buffer[0:n])
-
-		fmt.Println(message + " from id: " + strconv.Itoa(id))
-
-		var currentIndex = progressTracker[id]
-
-		if currentIndex == sendingExpressionLength {
-			connection.WriteToUDP([]byte("END"), remoteAddr)
-			fmt.Println("Finished sending expression to id: " + strconv.Itoa(id))
-			break
-		}
-
-		var nextLetter = string(sendingExpression[currentIndex]) + "\n"
-		connection.WriteToUDP([]byte(nextLetter), remoteAddr)
-
-		progressTracker[id] = currentIndex + 1
-	}
-
-	fmt.Println("A client disconnected. id: " + strconv.Itoa(id))
-	quit <- struct{}{}
-}
+var eventTime = time.Date(2025, 07, 29, 14, 30, 45, 100, time.Local)
 
 func main() {
-	arguments := os.Args
-	if len(arguments) == 1 {
-		fmt.Println("Please provide port number")
-		return
-	}
+        arguments := os.Args
+        if len(arguments) == 1 {
+                fmt.Println("Please provide a port number!")
+                return
+        }
+        PORT := ":" + arguments[1]
 
-	PORT := ":" + arguments[1]
-	s, err := net.ResolveUDPAddr("udp4", PORT)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+        s, err := net.ResolveUDPAddr("udp4", PORT)
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
 
-	connection, err := net.ListenUDP("udp4", s)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+        connection, err := net.ListenUDP("udp4", s)
+        if err != nil {
+                fmt.Println(err)
+                return
+        }
 
-	defer connection.Close()
+        defer connection.Close()
+        buffer := make([]byte, 1024)
 
-	progressTracker = make(map[int]int)
+        for {
+                n, addr, err := connection.ReadFromUDP(buffer)
+                fmt.Print("-> ", string(buffer[0:n-1]))
 
-	max_listeners := 5
-	quit := make(chan struct{})
+				var today = time.Now()
+				var timeBetween = int(eventTime.Sub(today).Seconds())
+				
 
-	for i := 0; i < max_listeners; i++ {
-		go handleConnection(connection, quit)
-	}
-	<-quit // Waits for an error to happen.
+                data := []byte(strconv.Itoa(timeBetween) + "\n")
+                fmt.Printf("data: %s\n", string(data))
+                _, err = connection.WriteToUDP(data, addr)
+                if err != nil {
+                        fmt.Println(err)
+                        return
+                }
+        }
 }
