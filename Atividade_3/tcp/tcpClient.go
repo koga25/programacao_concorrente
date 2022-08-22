c5a74ef9c64c10a6b63db9b2723917e39970b11d
André Luiz <alps2@cin.ufpe.br>
2022-08-21 22:46:17 -0300

    Testing start

--- Atividade_3/tcp/tcpClient.go
+++ Atividade_3/tcp/tcpClient.go
@@ -6,13 +6,14 @@ import (
 	"net"
 	"os"
 	"strconv"
+	"time"
 )
 
 func main() {
 	arguments := os.Args
 	if len(arguments) == 1 {
 		fmt.Println("Please provide host:port.")
-		return
+		os.Exit(0)
 	}
 
 	fmt.Println("Starting TCP connection at " + arguments[1])
@@ -21,18 +22,24 @@ func main() {
 	c, err := net.Dial("tcp", CONNECT)
 	if err != nil {
 		fmt.Println(err)
-		return
+		os.Exit(0)
 	}
 
+	var rttTotal = 0
 	var tries = 10000
 	for i := 0; i < tries; i++ {
+		var initialTime = time.Now()
 		fmt.Fprintf(c, "sendTimeBetween\n")
 
 		message, _ := bufio.NewReader(c).ReadString('\n')
-		
+		var finalTime = time.Now()
+
+		var rtt = int(finalTime.Sub(initialTime).Milliseconds())
+		rttTotal = rttTotal + rtt
+
 		if message == "END" {
 			fmt.Println("Server asked to disconnect. TCP client exiting...")
-			return
+			os.Exit(0)
 		}
 
 		fmt.Println(strconv.Itoa(i) + " ->: " + message)
@@ -40,4 +47,6 @@ func main() {
 
 	c.Write([]byte("END"))
 
+	rttTotal = rttTotal / tries
+	os.Exit(rttTotal)
 }
